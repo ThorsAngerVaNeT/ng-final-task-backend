cfdc156ec4fed24e592c91f06cccc751ddbcd082
Vadzim Antonau <vanet@tut.by>
2023-02-24 10:04:26 +0300

    feat: add env variables to mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 import { PORT } from './constants';
+import * as dotenv from 'dotenv';
+dotenv.config()
 
 import * as serverService from './services/server.service';
 
+const { DB_USERNAME, DB_PASSWORD, CLUSTER_URL } = process.env;
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect(`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${CLUSTER_URL}`);
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
